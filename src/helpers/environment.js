c49a960b344715d481e6820681c3fc45b9a91012
Irvin Lee <irvin.lee.work@gmail.com>
2021-12-02 11:48:03 -0500

    heroku deploy

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch(window.location.hostname) {
     case "localhost" || "127.0.0.1":
         APIURL = "http://localhost:3000";
         break;
-        case 'javacord.herokuapp.com':
-        APIURL = "https://javacord.herokuapp.com"
+    case 'javacord.herokuapp.com':
+        APIURL = "https://javacord-client.herokuapp.com"
 }
 export default APIURL;
