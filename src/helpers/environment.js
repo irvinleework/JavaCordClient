e9b6a303da1e3ae34fc18902f7b152c5a59dfa6e
Irvin Lee <irvin.lee.work@gmail.com>
2021-12-02 10:07:06 -0500

    heroku push

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,5 +4,7 @@ switch(window.location.hostname) {
     case "localhost" || "127.0.0.1":
         APIURL = "http://localhost:3000";
         break;
+        case 'javacord.herokuapp.com':
+        APIURL = "https://javacord.herokuapp.com/"
 }
 export default APIURL;
