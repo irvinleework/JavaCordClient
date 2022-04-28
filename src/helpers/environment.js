5905e66a69880c2070ebe38c058e740f4a26184d
Irvin Lee <irvin.lee.work@gmail.com>
2022-04-28 18:45:58 -0400

    changed port from 3000 to 4000 in apiurl

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -2,7 +2,7 @@ let APIURL = "";
 
 switch(window.location.hostname) {
     case "localhost" || "127.0.0.1":
-        APIURL = "http://localhost:3000";
+        APIURL = "http://localhost:4000";
         break;
     case "javacord-client.herokuapp.com":
         APIURL = "https://javacord.herokuapp.com"
